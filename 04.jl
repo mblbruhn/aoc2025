99ed19ca33cb9d6cbffa3e90be8d8bd9830f4880
Eudoxosfly <87175263+Eudoxosfly@users.noreply.github.com>
2025-12-04 18:07:13 +0100

    day 4

---
+++ 04.jl
@@ -0,0 +1,75 @@
+using PaddedViews
+using BenchmarkTools
+
+function string_to_matrix(input::Vector{String})
+    a = reduce(hcat, split.(input, ""))
+    d = Dict("." => 0, "@" => 1)
+    out::Array{Int8} = collect(map(x -> d[x], a)')
+    return out
+end
+
+function neighbor_map(in_map::Array{Int8})
+    rows, cols = in_map.size
+    out::Array{Int8} = zeros(Int8, (rows, cols))
+    pad_val::Int8 = 0
+    padded = PaddedView(pad_val, in_map, (0:rows+1, 0:cols+1))
+    for ii = 1:rows 
+        for jj = 1:cols
+            out[ii, jj] = sum(padded[ii-1:ii+1, jj-1:jj+1])
+        end
+    end
+    out = in_map .* out
+    return out
+end
+
+
+function part1(input)
+    numerical_input = string_to_matrix(input)
+    nm = neighbor_map(numerical_input)
+    thresh_lo::Int8 = 0
+    thresh_hi::Int8 = 5
+    movable = sum(thresh_lo .< nm .< thresh_hi)
+    return movable
+end
+
+function part2(input)
+    function remove_rolls!(coord)
+        neighboring = [(coord[1]+ii, coord[2]+jj) for ii = -1:1:1 for jj = -1:1:1]
+        n_neighbors = 0
+        for neighbor_coord in neighboring     
+            if neighbor_coord in coords
+                n_neighbors += 1
+            end
+        end
+        if n_neighbors < 5
+            pop!(coords2, coord)
+        end
+    end
+
+    # create set of coordinates with paper rolls 
+    coords = Set()
+    mat = reduce(hcat, split.(input, ""))
+    for jj = 1:mat.size[1]
+        for ii = 1:mat.size[2]
+            if mat[ii, jj] == "@"
+                push!(coords, (ii, jj))
+            end
+        end
+    end
+    removed = 0
+    coords2 = copy(coords)
+    remove_rolls!.(coords)
+    removed += length(coords) - length(coords2)
+    while coords != coords2
+        coords = copy(coords2)
+        coords2 = copy(coords)
+        remove_rolls!.(coords)
+        removed += length(coords) - length(coords2)
+    end
+    return removed
+end
+
+input = readlines(".data/04.txt")
+println("Part 1: $(part1(input)); Part 2: $(part2(input))")
+@benchmark (part1(input), part2(input))
+
--- README.md
+++ README.md
@@ -20,3 +20,11 @@ BenchmarkTools.Trial: 10000 samples with 1 evaluation per sample.
  Time  (median):     57.800 μs              ┊ GC (median):    0.00%
  Time  (mean ± σ):   65.867 μs ± 56.361 μs  ┊ GC (mean ± σ):  0.00% ± 0.00%
 ```
+
+Day 4:
+```
+BenchmarkTools.Trial: 30 samples with 1 evaluation per sample.
+ Range (min … max):  149.276 ms … 331.865 ms  ┊ GC (min … max):  7.10% … 57.80%
+ Time  (median):     163.424 ms               ┊ GC (median):    15.63%
+ Time  (mean ± σ):   170.730 ms ±  31.990 ms  ┊ GC (mean ± σ):  17.68% ±  8.25%
+```
